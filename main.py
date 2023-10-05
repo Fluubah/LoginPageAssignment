c80c3189945e1c754cdee6b42037ce9806bd15d2
Fluubah <75446686+Fluubah@users.noreply.github.com>
2023-10-05 10:20:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,18 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 Builder.load_file("LoginPage.kv")
 
 
+accounts = {"Aaron": "Boofin"}
+
+
+class LoginPageApp(App):
+
+    def build(self):
+        return LoginManager()
+
+
 class StartScreen(Screen):
-    pass
+    def next_page(self, screen_to):
+        self.manager.current = screen_to
 
 
 class LoginManager(ScreenManager):
@@ -14,4 +24,36 @@ class LoginManager(ScreenManager):
 
 
 class CreateScreen(Screen):
+    def create_account(self, username, password, password_confirm):
+        special = "~!@#$%^&*()_+-="
+        has_special = False
+        numbers = "1234567890"
+        has_numbers = False
+        has_capital = False
+        has_lowercase = False
+        for i in password.text:
+            if i in special:
+                has_special = True
+            if i in numbers:
+                has_numbers = True
+            if i.capitalize() == i:
+                has_capital = True
+            if i.lower() == i:
+                has_lowercase = True
+
+        if (username.text not in accounts) and (password.text == password_confirm.text) and has_special and has_numbers and has_capital and has_lowercase and (len(password.text) > 7):
+            accounts[username.text] = password.text
+            self.manager.current = "login"
+
+
+class LoginScreen(Screen):
+    def login(self, username, password):
+        if username.text in accounts and accounts[username.text] == password.text:
+            self.manager.current = "logged"
+
+
+class LoggedIn(Screen):
     pass
+
+
+LoginPageApp().run()
